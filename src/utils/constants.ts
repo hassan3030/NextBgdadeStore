ec2c8e4f16ecb01b294eb849ddaedff677554e6c
Hassan Hamdi Ashour <61333986+hassan3030@users.noreply.github.com>
2025-07-16 11:05:18 +0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,10 @@
 // constants variables
 export const ARTICLE_PER_PAGE = 6;
 
-const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
+// const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
+// const DEVELOPMENT_DOMAIN = "http://localhost:3000";
+const PRODUCTION_DOMAIN = "https://deel-deal-front.csiwm3.easypanel.host";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
-
 // change between build and production mode
 
 export const DOMAIN = process.env.NODE_ENV === 'production' 
